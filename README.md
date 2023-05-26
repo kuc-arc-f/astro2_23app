b8568017faecc418402dec0d8beb524e4f7e5ef7
中島浩二 <knaka0209@gmail.com>
2023-05-26 12:37:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+﻿# astro2_23app
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2023/05/24
+
+ update  :
+
+***
+### Summary
+
+Astro 2 +  Cloudflare D1, app sample
+
+***
+### setup
+* .env
+* PUBLIC_API_URL, PUBLIC_API_KEY
+* auth info: PUBLIC_BASIC_AUTH_USER, PUBLIC_BASIC_AUTH_PASSWORD
+
+```
+PUBLIC_BASIC_AUTH_USER = "hoge"
+PUBLIC_BASIC_AUTH_PASSWORD = "1111"
+
+PUBLIC_API_URL=http://localhost:8787
+PUBLIC_API_KEY="hoge123"
+
+```
+
+***
+### Start
+
+```
+npm i
+
+#start-dev
+
+yarn  dev
+
+# build
+
+yarn build
+```
+
+***
+* start
+
+* http://localhost:3000/
+
+***
+### blog
+
+https://zenn.dev/knaka0209/books/b5e25eac05b2c5
+
+***
+
---
+++ src/pages/er_chart/App.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+import CrudIndex from "./CrudIndex";
+let items = [], itemsTodos = [];
+//
+const startProc= async function() {
+	items = await CrudIndex.getList();
+	console.log(items);
+}
+startProc();
+//
+const search = async function() {
+	console.log("search");
+	items = await CrudIndex.search();
+	console.log(items);
+}
+
+</script>
+
+<div class="">
+    <h1>ErChart</h1>
+    <p>mermaid.js display possible.</p>
+    <hr class="my-1" />	
+    <a href={`/er_chart/create`} class="btn btn-primary">Create</a>
+    <hr class="my-1" />	
+    {#each items as item}
+    <div>
+        <h3>{item.title}</h3>
+        <p>ID : {item.id}
+            <a href={`/er_chart/show/${item.id}`} class="mx-2 btn btn-sm btn-outline-primary">Show
+            </a>
+            <a href={`/er_chart/edit/${item.id}`} class="btn">[ Edit ]
+            </a>			
+        </p>
+        <hr />
+    </div>
+    {/each}		
+</div>
+
+<style>
+</style>
+
+<!--
+-->
